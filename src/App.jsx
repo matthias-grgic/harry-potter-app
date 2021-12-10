988b68395dffcd549764c8089768df995d2c9f51
matthias-grgic <85082249+matthias-grgic@users.noreply.github.com>
2021-12-10 13:12:24 +0100

    function bau

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,36 @@
 import { useEffect, useState } from "react";
 
-
 function App() {
-
-  const [students, setStudents] = useState([])
+  const [students, setStudents] = useState([]);
 
   useEffect(() => {
-    fetch('http://hp-api.herokuapp.com/api/characters/students')
+    fetch("http://hp-api.herokuapp.com/api/characters/students")
       .then((response) => response.json())
       .then((studentsFromApi) => {
         const allStudents = studentsFromApi.map((student) => ({
           name: student.name,
+          house: student.house,
+          patronus: student.patronus,
+          eyeColour: student.eyeColour,
+          image: student.image,
         }));
-
-        console.log(allStudents)
-
-        setStudents(allStudents)
+        setStudents(allStudents);
       });
-      
   }, []);
+
+  function CharacterCards(props) {
+    return (
+      <section>
+        <ul>{allStudents}</ul>
+      </section>
+    );
+  }
 
   return (
     <div className="App">
-      
+      <CharacterCards name={allStudents} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
