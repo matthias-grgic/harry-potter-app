1e1f842d87cc32f3e9d809b212e7322cece13ffc
matthias-grgic <85082249+matthias-grgic@users.noreply.github.com>
2021-12-10 16:27:51 +0100

    fotossss

--- src/components/CharacterCard.jsx
+++ src/components/CharacterCard.jsx
@@ -1,15 +1,37 @@
+import styled from "styled-components";
+
 function CharacterCard({ student }) {
+  // const [ stateImage, setStateImage ] =useState(false);
+  //   src={stateImage ? 'one image' : 'another image'}
+
   const studentProps = student.map((student, index) => (
-    <section>
-      <h2>{student.name}</h2>
-      <li>{student.house}</li>
-      <li>{student.patronus}</li>
-      <li>{student.eyeColour}</li>
-      <div>{student.image}</div>
-    </section>
+    <SectionContainer>
+      <StyledImage src={student.image} alt="Kein Foto" />
+      <StyledArticle>
+        <h2>{student.name}</h2>
+        <p>{student.house}</p>
+        <p>{student.patronus}</p>
+        <p>{student.eyeColour}</p>
+      </StyledArticle>
+    </SectionContainer>
   ));
 
   return <div>{studentProps}</div>;
 }
 
 export default CharacterCard;
+
+const StyledImage = styled.img`
+  height: 250px;
+`;
+const SectionContainer = styled.section`
+  display: flex;
+  flex-wrap: wrap;
+  margin: 1rem;
+  background-color: ;
+`;
+
+const StyledArticle = styled.article`
+  margin-left: 1rem;
+  font-size: 1rem;
+`;
