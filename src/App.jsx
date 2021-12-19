010dd4de85a0d387585178a62e7d3ee58175e338
matthias-grgic <85082249+matthias-grgic@users.noreply.github.com>
2021-12-19 21:50:29 +0100

    filter

--- src/App.jsx
+++ src/App.jsx
@@ -1,132 +1,122 @@
-import styled from "styled-components";
-import GlobalStyles from "./GlobalStyles";
-import { useEffect, useState } from "react";
-import {saveToLocal, loadFromLocal} from './Library/localStorage';
-import CharacterCard from "./components/CharacterCard";
-import { Routes, Route, NavLink, Link} from 'react-router-dom';
-import Logo from "./components/Logo";
+import styled from "styled-components"
+import GlobalStyles from "./GlobalStyles"
+import { useEffect, useState } from "react"
+import { saveToLocal, loadFromLocal } from "./Library/localStorage"
+import CharacterCard from "./components/CharacterCard"
+import { Routes, Route, NavLink, Link } from "react-router-dom"
+import Logo from "./components/Logo"
 
 function App() {
-  const [students, setStudents] = useState([]);
-  /* console.log(students); */
-  useEffect(() => {
-    fetch("http://hp-api.herokuapp.com/api/characters/students")
-      .then((response) => response.json())
-      .then((studentsFromApi) => {
-        const allStudents = studentsFromApi.map((student, index) => {
-          return {
-            id: index+1,
-            name: student.name,
-            house: student.house,
-            patronus: student.patronus,
-            eyeColour: student.eyeColour,
-            image: student.image,
-          };
-        });
-        setStudents(allStudents);
-      });
-  }, []);
+    const [students, setStudents] = useState([])
+    /* console.log(students); */
+    useEffect(() => {
+        fetch("http://hp-api.herokuapp.com/api/characters/students")
+            .then((response) => response.json())
+            .then((studentsFromApi) => {
+                const allStudents = studentsFromApi.map((student, index) => {
+                    return {
+                        id: index + 1,
+                        name: student.name,
+                        house: student.house,
+                        patronus: student.patronus,
+                        eyeColour: student.eyeColour,
+                        image: student.image,
+                    }
+                })
+                setStudents(allStudents)
+            })
+    }, [])
 
-  console.log(students);
-  
-  const localStorageFavouriteCharacters = loadFromLocal('_favouriteCharacters');
-  const [favouriteCharacters, setFavouriteCharacters] = useState (localStorageFavouriteCharacters ?? []);
+    const localStorageFavouriteCharacters = loadFromLocal("_favouriteCharacters")
+    const [favouriteCharacters, setFavouriteCharacters] = useState(localStorageFavouriteCharacters ?? [])
 
     useEffect(() => {
-        saveToLocal('_favouriteCharacters', favouriteCharacters);
-      }, [favouriteCharacters]);
+        saveToLocal("_favouriteCharacters", favouriteCharacters)
+    }, [favouriteCharacters])
 
-      function addToFavourites (favouriteCharacterToAdd) {
-        if (favouriteCharacters.some(
-            (everyFavouriteCharacter) =>
-            everyFavouriteCharacter.id === favouriteCharacterToAdd.id
-        )
-          ) {
-          const updatedFavouriteCharacters = favouriteCharacters.filter(
-            (everyFavouriteCharacter) =>
-            everyFavouriteCharacter.id !== favouriteCharacterToAdd.id
-          );
-          setFavouriteCharacters(updatedFavouriteCharacters);
+    function addToFavourites(favouriteCharacterToAdd) {
+        if (favouriteCharacters.some((everyFavouriteCharacter) => everyFavouriteCharacter.id === favouriteCharacterToAdd.id)) {
+            const updatedFavouriteCharacters = favouriteCharacters.filter((everyFavouriteCharacter) => everyFavouriteCharacter.id !== favouriteCharacterToAdd.id)
+            setFavouriteCharacters(updatedFavouriteCharacters)
         } else {
-        setFavouriteCharacters([...favouriteCharacters, favouriteCharacterToAdd]);
-        } 
-      }
+            setFavouriteCharacters([...favouriteCharacters, favouriteCharacterToAdd])
+        }
+    }
 
-  return (
-    <div>
-      <header>
-      <Headline>
-        <Link to="/"> <Logo/></Link>
-      </Headline>
+    //SORTIER FUNCTION
+    let sortG = students.filter((students) => students.house === "Gryffindor")
+    let sortS = students.filter((students) => students.house === "Slytherin")
+    let sortH = students.filter((students) => students.house === "Hufflepuff")
+    let sortR = students.filter((students) => students.house === "Ravenclaw")
 
-      <NavLinkStyle>
-        <NavLink to="character-card">Charactercards</NavLink>
-        <NavLink to="favourite-characters">Favourite Characters</NavLink>
-      </NavLinkStyle>
-      </header>
+    return (
+        <div>
+            <header>
+                <Headline>
+                    <Link to="/">
+                        {" "}
+                        <Logo />
+                    </Link>
+                </Headline>
 
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <StyledH2>
-              Welcome to the best Harry Potter Fanpage in the world!
-            </StyledH2>
-          }
-        />{" "}
-        
-        <Route
-          path="character-card"
-          element={<CharacterCard student={students} onAddToFavourites={addToFavourites} favouriteCharacters={favouriteCharacters} />}
-        />
+                <NavLinkStyle>
+                    <NavLink to="favourite-characters">Favourites 🤍</NavLink>
+                    <NavLink to="character-card">All Characters</NavLink>
+                    <NavLink to="sortG">Gryffindor</NavLink>
+                    <NavLink to="sortS">Slytherin</NavLink>
+                    <NavLink to="sortH">Hufflepuff</NavLink>
+                    <NavLink to="sortR">Ravenclaw</NavLink>
+                </NavLinkStyle>
+            </header>
 
-        <Route
-          path="favourite-characters"
-          element={<CharacterCard student={favouriteCharacters} onAddToFavourites={addToFavourites} favouriteCharacters={favouriteCharacters}/>}
-        />
-      </Routes>
-
-      
-    </div>
-  );
+            <Routes>
+                <Route path="/" element={<StyledH2>Welcome to the best Harry Potter Fanpage in the world!</StyledH2>} />{" "}
+                <Route path="character-card" element={<CharacterCard student={students} onAddToFavourites={addToFavourites} favouriteCharacters={favouriteCharacters} />} />
+                <Route path="favourite-characters" element={<CharacterCard student={favouriteCharacters} onAddToFavourites={addToFavourites} favouriteCharacters={favouriteCharacters} />} />
+                <Route path="sortG" element={<CharacterCard student={sortG} onAddToFavourites={addToFavourites} favouriteCharacters={favouriteCharacters} />} />
+                <Route path="sortS" element={<CharacterCard student={sortS} onAddToFavourites={addToFavourites} favouriteCharacters={favouriteCharacters} />} />
+                <Route path="sortH" element={<CharacterCard student={sortH} onAddToFavourites={addToFavourites} favouriteCharacters={favouriteCharacters} />} />
+                <Route path="sortR" element={<CharacterCard student={sortR} onAddToFavourites={addToFavourites} favouriteCharacters={favouriteCharacters} />} />
+            </Routes>
+        </div>
+    )
 }
 
-export default App;
+export default App
 
 const Headline = styled.h1`
-  background-color: var(--Gryffindor);
-  font-size: 2rem;
-  text-align: center;
-  a {
-  text-decoration: none;
-  }
-`;
+    background-color: var(--Gryffindor);
+    font-size: 2rem;
+    text-align: center;
+    a {
+        text-decoration: none;
+    }
+`
 
 const NavLinkStyle = styled.a`
-  
-  display: flex;
-  font-size: 1.2rem;
-  justify-content: space-around;
-  a {
-    background-color: var(--Hufflepuff);
-    border: 2px solid var(--Gryffindor);
-    border-radius: 10px;
-    color: var(--Gryffindor);
-    text-decoration: none;
-    padding: 0.5rem;
-    &:hover {
-    background-color: var(--Slytherin);
-    color: var(--Hufflepuff);
+    display: flex;
+    font-size: 1.2rem;
+    justify-content: space-around;
+    a {
+        background-color: var(--Hufflepuff);
+        border: 2px solid var(--Gryffindor);
+        border-radius: 10px;
+        color: var(--Gryffindor);
+        text-decoration: none;
+        padding: 0.5rem;
+        &:hover {
+            background-color: var(--Slytherin);
+            color: var(--Hufflepuff);
+        }
     }
-  }
-`;
+`
 
 const StyledH2 = styled.h2`
-  background-color: var(--Gryffindor);
-  color: white;
-  border-radius: 8px;
-  border: 4px solid var(--Hufflepuff);
-  text-align: center;
-  margin: 1rem 3rem;
-  padding: 0.8rem;
-`;
+    background-color: var(--Gryffindor);
+    color: white;
+    border-radius: 8px;
+    border: 4px solid var(--Hufflepuff);
+    text-align: center;
+    margin: 1rem 3rem;
+    padding: 0.8rem;
+`
--- src/components/CharacterCard.jsx
+++ src/components/CharacterCard.jsx
@@ -1,59 +1,67 @@
-import styled from "styled-components";
-import ToggleButton from "./ToggleButton";
+import styled from "styled-components"
+import ToggleButton from "./ToggleButton"
 
+function CharacterCard({ student, onAddToFavourites, favouriteCharacters }) {
+    const studentProps = student.map((student, index) => (
+        <SectionContainer key={index}>
+            <StyledImage
+                src={student.image}
+                alt="Kein Foto"
+                style={{
+                    border:
+                        student.house === "Gryffindor"
+                            ? "4px solid var(--Gryffindor)"
+                            : student.house === "Slytherin"
+                            ? "4px solid var(--Slytherin)"
+                            : student.house === "Hufflepuff"
+                            ? "4px solid var(--Hufflepuff)"
+                            : student.house === "Ravenclaw"
+                            ? "4px solid var(--Ravenclaw)"
+                            : "4px solid black",
+                }}
+            />
+            <StyledArticle>
+                <StyledHeadline>
+                    {student.name}
+                    <FavouritesIcon onClick={() => onAddToFavourites(student)}>{favouriteCharacters.some((favourite) => favourite.id === student.id) ? "⭐️" : "🪄"}</FavouritesIcon>
+                </StyledHeadline>
+                <ToggleButton house={student.house} patronus={student.patronus} eyeColour={student.eyeColour} />
+            </StyledArticle>
+        </SectionContainer>
+    ))
 
-function CharacterCard({student, onAddToFavourites, favouriteCharacters}) {
-
-  const studentProps = student.map((student, index) => (
-    <SectionContainer key={index}>
-      <StyledImage src={student.image} alt="Kein Foto" style={{border: student.house === "Gryffindor" ? "4px solid var(--Gryffindor)" : student.house === "Slytherin" ? "4px solid var(--Slytherin)" : student.house === "Hufflepuff" ? "4px solid var(--Hufflepuff)" : student.house === "Ravenclaw" ? "4px solid var(--Ravenclaw)" : "4px solid black"}}/>
-      <StyledArticle>
-        <StyledHeadline>{student.name}<FavouritesIcon 
-        onClick={() => onAddToFavourites(student)}>{favouriteCharacters.some((favourite) => favourite.id === student.id)
-        ? '⭐️'
-        : '🪄' }
-        </FavouritesIcon>
-        </StyledHeadline>
-        <ToggleButton house={student.house} patronus={student.patronus} eyeColour={student.eyeColour}/>
-      </StyledArticle>
-    </SectionContainer>
-  ));
-
-  return (
-  <div>{studentProps} 
-  </div>
-  );
+    return <div>{studentProps}</div>
 }
 
-export default CharacterCard;
+export default CharacterCard
 
 const StyledImage = styled.img`
-  height: 200px;
-  padding: 1px;
-`;
+    height: 200px;
+    padding: 1px;
+`
 
 const SectionContainer = styled.section`
-  display: flex;
-  flex-wrap: wrap;
-  margin: 1rem;
-`;
+    display: flex;
+    flex-wrap: wrap;
+    margin: 1rem;
+`
 
 const StyledArticle = styled.article`
-  margin-left: 1rem;
-  font-size: 1rem;
-  text-align: left;
-`;
+    margin-left: 1rem;
+    font-size: 1rem;
+    text-align: left;
+`
 
 const StyledHeadline = styled.h2`
-  font-size: 1.1rem;
-  font-weight: bold;
-  text-decoration: underline;
-  margin-bottom: 8px;
-`;
+    font-size: 1.1rem;
+    font-weight: bold;
+    text-decoration: underline;
+    margin-bottom: 8px;
+`
 
 const FavouritesIcon = styled.span`
-  font-size: 1.5rem;
-  right: 0.5rem;
-  bottom: 0.2rem;
-  cursor: pointer;
-`;
+    font-size: 1.5rem;
+    right: 0.5rem;
+    bottom: 0.2rem;
+    cursor: pointer;
+`
